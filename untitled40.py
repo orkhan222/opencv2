c341fa95f6682537118e7c03d1f710967e0debca
Orkhan Mustafayev <orkhanmustafayev44@gmail.com>
2022-01-06 13:11:41 +0400

    40

--- untitled40.py
+++ untitled40.py
@@ -6,11 +6,36 @@
 import cv2
 import numpy as np
 
+cizim = False
+mod = False
+xi,yi = -1,-1
+
+
 def draw(event, x, y,flags,param):
-    if event == cv2.EVENT_LBUTTONDBLCLK:
-        cv2.circle(img, (x,y), 50, (255,0,0),-1)
+    #if event == cv2.EVENT_LBUTTONDBLCLK:
+        #cv2.circle(img, (x,y), 50, (255,0,0),-1)
         
-    pass
+        global cizim,xi,yi,mod
+        
+        if event == cv2.EVENT_LBUTTONDBLCLK:
+            xi,yi = x,y
+            cizim = True
+        elif event == cv2.EVENT_MOUSEMOVE:
+            if cizim == True:
+                if mod:
+                    cv2.circle(img, (x,y), 10, (100,50,0),-1)
+                else:
+                    cv2.rectangle(img, (xi,yi), (x,y), (0,0,255),-1)
+            else:
+                pass
+            
+            
+        elif event == cv2.EVENT_LBUTTONUP:
+            cizim = False
+            if mod:
+                cv2.circle(img, (x,y), 10, (100,50,0),-1)
+            else:
+                cv2.rectangle(img, (xi,yi), (x,y), (0,0,255),-1)
 
 
 img = np.ones((512,512,3),np.uint8)
@@ -23,6 +48,7 @@ while (1):
     cv2.imshow('paint', img)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
-    
+    if cv2.waitKey(1) & 0xFF == ord('m'):
+        mod = not mod
     
 cv2.destroyAllWindows()
